23a42261e4fc9c6a58868d07c709b859dd6867e7
s2monx <zzprosweet@gmail.com>
2019-09-09 11:31:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bai2/MainActivity.java
@@ -0,0 +1,155 @@
+package com.example.bai2;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtAL;
+    EditText etxtDL;
+    Button btnAL;
+
+    int year;
+
+    String can;
+    String chi;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        txtAL =  (TextView) findViewById(R.id.txtAL);
+        etxtDL =  (EditText) findViewById(R.id.etxtDL);
+        btnAL =  (Button) findViewById(R.id.btnAL);
+
+
+
+
+
+        btnAL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                year = Integer.parseInt(etxtDL.getText().toString());
+                // Xác định Can
+                switch (year % 10) {
+                    case 0:
+                        can = "Canh";
+                        break;
+                    case 1:
+                        can = "Tân";
+                        break;
+                    case 2:
+                        can = "Nhâm";
+                        break;
+                    case 3:
+                        can = "Quý";
+                        break;
+                    case 4:
+                        can = "Giáp";
+                        break;
+                    case 5:
+                        can = "Ất";
+                        break;
+                    case 6:
+                        can = "Bính";
+                        break;
+                    case 7:
+                        can = "Đinh";
+                        break;
+                    case 8:
+                        can = "Mậu";
+                        break;
+                    case 9:
+                        can = "Kỷ";
+                        break;
+                }
+
+                // Xác định Chi
+                switch (year % 12) {
+                    case 0:
+                        chi = "Thân";
+                        break;
+                    case 1:
+                        chi = "Dậu";
+                        break;
+                    case 2:
+                        chi = "Tuất";
+                        break;
+                    case 3:
+                        chi = "Hợi";
+                        break;
+                    case 4:
+                        chi = "Tý";
+                        break;
+                    case 5:
+                        chi = "Sửu";
+                        break;
+                    case 6:
+                        chi = "Dần";
+                        break;
+                    case 7:
+                        chi = "Mẹo";
+                        break;
+                    case 8:
+                        chi = "Thìn";
+                        break;
+                    case 9:
+                        chi = "Tỵ";
+                        break;
+                    case 10:
+                        chi = "Ngọ";
+                        break;
+                    case 11:
+                        chi = "Mùi";
+                        break;
+                }
+
+                txtAL.setText("Năm âm lịch tương ứng với năm dương lịch " +year+ " là " + (can+ " " +chi));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
